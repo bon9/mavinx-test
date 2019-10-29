bd24bf2e277e754816ecd0ff4850a1f474d82377
Oleh <boshaa3@gmail.com>
2019-10-29 14:48:11 +0200

    Updates

---
+++ precache-manifest.80107b5eeee1b4c93413e50ddf1effd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709b716d632498fe1ac04c7c90e21e71",
+    "url": "/mavinx-test/index.html"
+  },
+  {
+    "revision": "cfad8c5ff6f710ebfd4d",
+    "url": "/mavinx-test/static/css/main.1925a05b.chunk.css"
+  },
+  {
+    "revision": "3c8c77c3d6e8c60fd180",
+    "url": "/mavinx-test/static/js/2.cb7e1e8f.chunk.js"
+  },
+  {
+    "revision": "cfad8c5ff6f710ebfd4d",
+    "url": "/mavinx-test/static/js/main.28696860.chunk.js"
+  },
+  {
+    "revision": "b752d25623f14956a050",
+    "url": "/mavinx-test/static/js/runtime-main.9ba46212.js"
+  }
+]);
